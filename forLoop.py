aad830d910cb5a553ff3e00a9f9ec47e71b04fa5
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2019-12-06 02:54:33 +0300

    Delete forLoop.py

--- forLoop.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-friends: List[str] = ['john', 'pat', 'gary', 'michael']
-name: str
-for i, name in enumerate(friends):
-    print("iteration {iteration} is {name}".format(iteration=i, name=name))
