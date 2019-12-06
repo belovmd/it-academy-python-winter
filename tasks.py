1f99f657fbd720ba514442d2a3c48141448ac37b
Igor Bohdan <igor3407.b@gmail.com>
2019-12-06 13:00:33 +0300

    finally

--- tasks.py
+++ tasks.py
@@ -1,19 +1,11 @@
-﻿print('Hello, world!')
+﻿print("Hello Word")
+
 name = input('What is your name?\n')
-print('Hi, %s.'% name')
+
+print('Hi, %s.'% name)
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print("iteration {iteration} is {name}".format(iteration=i, name=name))
-parents, babies = (1, 1)
-while babies < 100:
-    print('This generation has {0} babies'.format(babies))
-    parents, babies = (babies, parents + babies)
-def greet(name):
-    print('Hello', name)
-
-greet('Jack')
-greet('Jill')
-greet('Bob')
 
 
 
