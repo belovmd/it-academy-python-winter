f26c397b7b07e545634727bf225c013563e967e7
Paromenskaya Marina <paromenskaya@gmail.com>
2019-12-07 13:03:37 +0300

    HomeWork1: Marina Paromenskaya

---
+++ HomeWork1/HW_1.py
@@ -0,0 +1,25 @@
+# 1 - Определить, делится ли число на 17.
+num = input('Input a number:\n')
+num = int(num)
+if num % 17 == 0:
+    print('%d is divided by 17.' % num)
+else:
+    print('%d is not divided by 17.' % num)
+
+# 2 - Определить, является ли введенное число простым.
+num = input('Input a number:\n')
+num = int(num)
+for i in range(2, num - 1):
+    if num % i == 0:
+        print('%d is not a simple number...' % num)
+        exit()
+print('%d is a simple number!' % num)
+
+# 3 - Определить, является ли число палиндромом.
+num = input('Input a number:\n')
+num_length = len(num)//2
+for i in range(0,  num_length - 1):
+    if num[i] != num[len(num) - 1 - i]:
+        print('%s is not a palindrome...' % num)
+        exit()
+print('%s is a palindrome!' % num)
