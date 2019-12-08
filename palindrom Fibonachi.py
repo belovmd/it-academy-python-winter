c0d58c3e18d8fc723cbf02f566660fc4755fcb63
renat33 <58603739+renat33@users.noreply.github.com>
2019-12-08 17:41:00 +0300

    Update palindrom Fibonachi.py

--- palindrom Fibonachi.py
+++ palindrom Fibonachi.py
@@ -1,9 +1,9 @@
 userNum = int(input("введите число\n"))
 
-for i in range(1, userNum+1):
-    print(i**3)
+for i in range(1, userNum + 1):
+    print(i ** 3)
 for a in range(userNum):
-    if userNum % (a+2) == 0 and a != userNum-2:
+    if userNum % (a + 2) == 0 and a != userNum - 2:
         print("не очень-то и простое")
         break
     print("простое")
@@ -12,15 +12,15 @@ for a in range(userNum):
 fib0, fib1 = 0, 1  # числа Фибоначчи
 
 for u in range(userNum):
-    a = fib0+fib1
+    a = fib0 + fib1
     fib0 = fib1
     fib1 = a
     print(fib1)
 
-leng=len(str(userNum)) # палиндром
+leng=len(str(userNum))  # палиндром
 if leng != 1:
     for j in range(leng):
-        if str(userNum)[j]==str(userNum)[leng-1-j]:
+        if str(userNum)[j] == str(userNum)[leng - 1 - j]:
             pass
         else:
             print("число не является палиндромом")
