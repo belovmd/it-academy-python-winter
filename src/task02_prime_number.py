a9d455310260b1b1efe3019fcc0ef8d81343c5b8
vorongn <vorongn@gmail.com>
2019-12-09 13:07:31 +0300

    fix

--- src/task02_prime_number.py
+++ src/task02_prime_number.py
@@ -1,7 +1,7 @@
 # простое число
 num = int(input('Введите число: '))
 if num > 1:
-    for i in range(2, num-1):
+    for i in range(2, num - 1):
         if num % i == 0:
             print('     ', num, '- не простое')
             break
