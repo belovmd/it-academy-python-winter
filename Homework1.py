09b9ac48bfccebf1c6ff50a2c91dea5246b6afa1
Dzmitry89 <bachkovskiydv@gmail.com>
2019-12-10 00:08:42 +0300

    correction error 2

--- Homework1.py
+++ Homework1.py
@@ -1,6 +1,6 @@
-import re
 import sys
 import glob
+import re
 from time import localtime
 # 1 line: Output
 print('Hello, world!')
@@ -38,7 +38,8 @@ my_purchase = {'apple': 1, 'banana': 6}
 grocery_bill = sum(prices[fruit] * my_purchase[fruit] for fruit in my_purchase)
 print('I owe the grocer $%.2f' % grocery_bill)
 # 8 lines: Command line arguments, exception handling
-# This program adds up integers that have been passed as arguments in the command line
+# This program adds up integers that have been passed as arguments
+# in the command line
 try:
     total = sum(int(arg) for arg in sys.argv[1:])
     print('sum =', total)
