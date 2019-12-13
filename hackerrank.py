3718461256ead60c61fbedf1ed61ac5cb54764f5
renat33 <58603739+renat33@users.noreply.github.com>
2019-12-13 23:51:23 +0300

    Update hackerrank.py

--- hackerrank.py
+++ hackerrank.py
@@ -1,9 +1,13 @@
 # You are given a string. Split the string on a " " (space)
 # delimiter and join using a - hyphen.
+
+
 def split_and_join(line):
     line = line.split()
     line = "-".join(line)
     return line
+
+
 # блок ниже был уже в задаче
 if __name__ == '__main__':
     line = input()
