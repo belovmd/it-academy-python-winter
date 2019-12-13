3c229d42836d2e8b577ddcdfb4754c927f172f74
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2019-12-13 22:24:22 +0300

    Delete longestWord.py

--- src/longestWord.py
+++
@@ -1,7 +0,0 @@
-sentence = str(input("Please, enter a sentence: "))
-longestWord = 0
-sentence = sentence.split(",")
-for word in range(1, len(sentence)):
-    if len(sentence[longestWord]) < len(sentence):
-        longestWord = i
-print(sentence[longestWord])
