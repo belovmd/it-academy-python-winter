bf495acf4ee66ce544fb43842a14bfdf3d51fa32
renat33 <58603739+renat33@users.noreply.github.com>
2019-12-13 23:52:44 +0300

    Update Hackerrank3.py

--- Hackerrank3.py
+++ Hackerrank3.py
@@ -1,11 +1,15 @@
 # Read a given string, change the character at a given index
 # and then print the modified string.
 # replace the character at index i with character c.
+
+
 def mutate_string(string, position, character):
     spis = list(s)
     spis[int(i)] = c
-    spis="".join(spis)
+    spis = "".join(spis)
     return(spis)
+
+
 # блок ниже был уже в задаче
 if __name__ == '__main__':
     s = input()
