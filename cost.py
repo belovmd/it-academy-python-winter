2a7a3b6db26c346d38ccfaf1ae4511836690ca3d
renat33 <58603739+renat33@users.noreply.github.com>
2019-12-13 22:15:48 +0300

    Update cost.py

--- cost.py
+++ cost.py
@@ -1,7 +1,7 @@
 m = int(input("Введите кол-во рублей\n"))
 n = int(input("Введите кол-во копеек\n"))
-l = int(input("Введите кол-во покупаемого товара\n"))
-costn = n * l / 100
-cost = costn + (m * l)
+numGoods = int(input("Введите кол-во покупаемого товара\n"))
+costn = n * numGoods / 100
+cost = costn + (m * numGoods)
 print("Общая цена составляет", cost, "рублей")
 print("Или", cost // 1, "руб.", round((cost - (cost // 1)) * 100), "коп.")
