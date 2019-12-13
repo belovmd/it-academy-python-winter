df7bc8ebc9db85c59a785fbd58836b0355f75bad
alex-shef <58556662+alex-shef@users.noreply.github.com>
2019-12-13 13:48:48 +0200

    Homework №2 and reorganizing the repository

--- 2nd lecture/task1.py
+++ 2nd lecture/task1.py
@@ -5,6 +5,7 @@ if (var / 17) % 2 == 0:
         print("маловато")
     elif var > 0:
         print("нормально")
-    else: print("на нет спроса нет")
+    else:
+        print("на нет спроса нет")
 else:
     print("ого")
--- 2nd lecture/task2.py
+++ 2nd lecture/task2.py
@@ -1,9 +1,10 @@
 import math
+
 a = float(input("1ая сторона ", ))
 b = float(input("2ая сторона ", ))
-c = float(input("3ая сторона ", ))
-p = (a+b+c)/2
-if a+b>c or a+c>b or b+c>a:
-    print('s =', math.sqrt(p*(p-a)*(p-b)*(p-c)))
+c = float(input("3ья сторона ", ))
+p = (a + b + c) / 2
+if a + b > c or a + c > b or b + c > a:
+    print("s =", math.sqrt(p * (p - a) * (p - b) * (p - c)))
 else:
-    print('Это не треугольник')
+    print("Это не треугольник")
