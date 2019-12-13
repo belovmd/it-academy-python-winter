c0a432a0fb155a7fdd5c2560cb6ac1d0ad4efd98
vorongn <vorongn@gmail.com>
2019-12-13 22:31:13 +0300

    fix

--- src/task02_04_massiv_stepen.py
+++ src/task02_04_massiv_stepen.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     assert index_power([1, 3, 10, 100], 3) == 1000000, "Cube"
     assert index_power([0, 1], 0) == 1, "Zero power"
     assert index_power([1, 2], 3) == -1, "IndexError"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+    print("Coding complete? Click 'Check' "
+          "to review your tests and earn cool rewards!")
