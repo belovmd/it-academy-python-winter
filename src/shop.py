1f763781fb746647d50372359d7fc59620bfa568
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2019-12-13 22:24:41 +0300

    Delete shop.py

--- src/shop.py
+++
@@ -1,4 +0,0 @@
-m = float(input("Enter a price of product: ")) # пользователь вводит ценн
-n = int(input("Enter a number of products: ")) # пользователь вводит кол-во продуктов
-main_sum = m * n # умножаем m и n
-print(main_sum)
