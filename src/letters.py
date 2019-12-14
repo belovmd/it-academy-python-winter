89a1949d1d59422136b32e672ccc49c08a5540bb
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2019-12-14 14:09:19 +0300

    Update letters.py

--- src/letters.py
+++ src/letters.py
@@ -4,7 +4,7 @@ bigLetters = 0
 for i in sentence:
     if 'a' <= i <= 'z':
         smallLetters += 1
-    elif 'A' <= i <= 'Z':
+ elif 'A' <= i <= 'Z':
             bigLetters += 1
 print(smallLetters)
 print(bigLetters)
