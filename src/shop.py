da2f8747c87cb91dbfcffdab2303221e79ef3e81
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2019-12-14 14:14:11 +0300

    Update shop.py

--- src/shop.py
+++ src/shop.py
@@ -1,4 +1,4 @@
-m = float(input("Enter a price of product: "))    # пользователь вводит ценн
-n = int(input("Enter a number of products: "))    # пользователь вводит кол-во продуктов
-main_sum = m * n    # умножаем m и n
+m = float(input("Enter a price of product: "))   # пользователь вводит ценн
+n = int(input("Enter a number of products: "))   # пользователь вводит кол-во прод
+main_sum = m * n   # умножаем m и n
 print(main_sum)
