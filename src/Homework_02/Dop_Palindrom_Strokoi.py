453e2393d845e132f7ab1b6d8ad7cc49def18eec
eldar-dniaprouski <eldar.dnep@gmail.com>
2019-12-18 21:10:52 +0300

    Fixed (removed redundant prints)

--- src/Homework_02/Dop_Palindrom_Strokoi.py
+++ src/Homework_02/Dop_Palindrom_Strokoi.py
@@ -2,8 +2,6 @@
 my_string = 'aaa bbbb aa bbb c bbbaa bb b b aaa'
 print(my_string)
 my_rep_string = my_string.replace(' ', '')
-print(my_rep_string)  # print просто для красоты
-print(my_rep_string[::-1])  # print просто для красоты
 if my_rep_string == my_rep_string[::-1]:
     print('Палиндром')
 else:
