2986070632b933664480a192e913c23b387b8f4c
eldar-dniaprouski <eldar.dnep@gmail.com>
2019-12-18 22:09:22 +0300

    Palindrom v2 (revert number using arithmetic operators)

--- src/Homework_02/Dop_Palindrom.py
+++ src/Homework_02/Dop_Palindrom.py
@@ -1,10 +1,10 @@
-'''
+"""
 Определите, является ли число палиндромом
 (читается слева направо и справа налево одинаково).
 Число положительное целое, произвольной длины.
 Задача требует работать только с числами
 (без конвертации числа в строку)
-'''
+"""
 
 num = int(input('Введите число\n'))
 num_length = 1
@@ -14,6 +14,9 @@ while num_division // 10 > 0:
     num_division //= 10
 print('Длина числа', num_length)
 
+num_length_2 = num_length  # для второго варианта
+
+# Вариант 1
 num_x = num
 for i in range(num_length // 2):
     n1 = num_x // 10 ** (num_length - 1)
@@ -26,3 +29,15 @@ for i in range(num_length // 2):
         break
 else:
     print(num, 'палиндром')
+
+# Вариант 2 (revert number using arithmetic operators)
+new_num = 0
+cur_num = num
+for x in range(num_length_2):
+    n_end = cur_num % 10
+    cur_num = (cur_num - n_end) // 10
+    new_num = new_num * 10 + n_end
+if num == new_num:
+    print(num, 'палиндром')
+else:
+    print(num, 'не палиндром')
