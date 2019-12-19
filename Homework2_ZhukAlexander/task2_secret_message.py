22693cbcc601a8f7ab712b55006fa5b1d23baf8e
ZhukAlexander90 <earl.j.hickey0@gmail.com>
2019-12-19 08:03:14 +0300

    fix pycodestyle Error - homework 1 in this branch too. At the moment I do not know how to fix it

--- Homework2_ZhukAlexander/task2_secret_message.py
+++ Homework2_ZhukAlexander/task2_secret_message.py
@@ -20,8 +20,11 @@ if __name__ == '__main__':
     print('Example:')
     print(find_message("How are you? Eh, ok. Low or Lower? Ohhh."))
 
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert find_message("How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
+    # These "asserts" using only for self-checking
+    # and not necessary for auto-testing
+    assert find_message("How are you? Eh, ok. "
+                        "Low or Lower? Ohhh.") == "HELLO", "hello"
     assert find_message("hello world!") == "", "Nothing"
     assert find_message("HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+    print("Coding complete? Click 'Check' to review "
+          "your tests and earn cool rewards!")
