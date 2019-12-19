5abc634242aebf50145317f73af93c0b744ebb77
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2019-12-19 20:30:11 +0300

    Update pyCheckio.py

--- src/homework2/pyCheckio.py
+++ src/homework2/pyCheckio.py
@@ -32,8 +32,7 @@ def easy_unpack(elements: tuple) -> tuple:
     """
         returns a tuple with 3 elements - first, third and second to the last
     """
-
-
+    
     return (elements[0], elements[2], elements[-2])
 
 
@@ -63,7 +62,7 @@ if __name__ == '__main__':
     assert index_power([1, 3, 10, 100], 3) == 1000000, "Cube"
     assert index_power([0, 1], 0) == 1, "Zero power"
     assert index_power([1, 2], 3) == -1, "IndexError"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+    print("Click 'Check' to review your tests and earn cool rewards!")
 
 
 def checkio(number: int) -> int:
