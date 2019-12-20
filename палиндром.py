cb290478f0addb822cb01aadc8faf419fb6e58f0
renat33 <58603739+renat33@users.noreply.github.com>
2019-12-20 20:50:03 +0300

    Update палиндром.py

--- палиндром.py
+++ палиндром.py
@@ -3,24 +3,18 @@ userPal = int(input("Введите претендента на палиндро
 if userPal <= 9:
     print("Слишком маленькое число")
     userPal = int(input("Введите число больше 9\n"))
-# посчитаем количество разрядов циклом while
-# введём доп переменную, чтобы не переписывать значение userPal
-razr = -1
-razrPal = 1
-newPal = userPal
-while razrPal != 0:
-    razrPal = newPal // 10
-    newPal = razrPal
-    razr += 1
-# введём доп переменную, чтобы снова не переписывать значение в userPal
+
 userPalvrem = userPal
-# введём переменную для хранения "числа-наоборот"
-userPal1 = 0
-for i in range(razr, -1, -1):
+userRevers = 0
+userPalvrem1 = userPal
+
+while userPalvrem1 * 10 // 10 != 0:
+    userPalvrem1 //= 10
     low = userPalvrem % 10
     userPalvrem //= 10
-    userPal1 += low * 10 ** i
-if userPal == userPal1:
+    userRevers = userRevers * 10 + low
+
+if userPal == userRevers:
     print("Поздравляю! Ваше число является палиндромом")
 else:
     print("Ваше число не является палиндромом")
