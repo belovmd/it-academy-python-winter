28f7301995d8eab9b876124586ce7f387fca8aa6
renat33 <58603739+renat33@users.noreply.github.com>
2019-12-20 19:54:41 +0300

    Update and rename the longest word.py to theLongestWord.py

--- theLongestWord.py
+++ theLongestWord.py
@@ -1,17 +1,17 @@
 user = str(input())
-trash = (".,!?:;'[]()-  … – «»")
+trash = (".,!?:;'[]()-}{ /\ … – «»")
 # заменяем знаки препинания на пробелы
-for i in range(len(trash)):
-    user = user.replace(trash[i], " ")
+for el in trash:
+    user = user.replace(el, " ")
 # создаём список из слов и узнаём длинну первого слова
 spis = user.split()
 a1 = len(spis[0])
 # сравниваем длинну первого слова с длинной каждого элемента списка
 # после всех итераций у нас в lword остаётся самое длинное слово
-for i in range(len(spis)):
-    if a1 <= len(spis[i]):
-        a1 = len(spis[i])
-        lword = spis[i]
+for word in spis:
+    if a1 <= len(word):
+        a1 = len(word)
+        lword = word
 
 print("Самое длинное слово(а):", end=" ")
 # находим слова, которые равны длинне самого длинного и выводим их
