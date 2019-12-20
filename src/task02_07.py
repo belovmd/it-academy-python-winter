eb079f9d1a849b5d8efafa8ea6ba16112702db11
vorongn <vorongn@gmail.com>
2019-12-20 12:44:49 +0300

    fix long line

--- src/task02_07.py
+++ src/task02_07.py
@@ -35,4 +35,5 @@ if __name__ == '__main__':
     assert checkio(6) == "Fizz", "6 is divisible by 3"
     assert checkio(5) == "Buzz", "5 is divisible by 5"
     assert checkio(7) == "7", "7 is not divisible by 3 or 5"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+    print("Coding complete? Click 'Check' to review your "
+          "tests and earn cool rewards!")
--- src/task02_08.py
+++ src/task02_08.py
@@ -38,4 +38,5 @@ if __name__ == '__main__':
     assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
     assert checkio([6]) == 36, "(6)*6=36"
     assert checkio([]) == 0, "An empty array = 0"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+    print("Coding complete? Click 'Check' to review your tests "
+          "and earn cool rewards!")
