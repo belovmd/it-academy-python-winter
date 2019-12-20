c32001eae99315c0f70cfa357d12a32cd115f746
renat33 <58603739+renat33@users.noreply.github.com>
2019-12-20 19:38:08 +0300

    Update Hackerrank2.py
    used local variables

--- Hackerrank2.py
+++ Hackerrank2.py
@@ -4,8 +4,7 @@
 
 
 def print_full_name(a, b):
-    print("Hello {0} {1}! "
-          "You just delved into python.".format(first_name, last_name))
+    print("Hello", a, b + "! You just delved into python.")
 
 
 # блок ниже уже был в задаче
