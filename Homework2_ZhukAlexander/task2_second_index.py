62bcb5dc934b707ca17a80bdba97116450dff884
ZhukAlexander90 <earl.j.hickey0@gmail.com>
2019-12-21 19:34:18 +0300

    correct remarks + tasks py.checkio.org

---
+++ Homework2_ZhukAlexander/task2_second_index.py
@@ -0,0 +1,26 @@
+# Вам даны две строки, и вы должны
+# найти индекс второго вхождения второй строки в первой.
+
+
+def second_index(text: str, symbol: str) -> [int, None]:
+    if text.count(symbol) >= 2:
+        first = text.find(symbol)
+        second = text.find(symbol, first+1)
+        return second
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(second_index("sims", "s"))
+
+    # These "asserts" are used for
+    # self-checking and not for an auto-testing
+    assert second_index("sims", "s") == 3, "First"
+    assert second_index("find the river", "e") == 12, "Second"
+    assert second_index("hi", " ") is None, "Third"
+    assert second_index("hi mayor", " ") is None, "Fourth"
+    assert second_index("hi mr Mayor", " ") == 5, "Fifth"
+    print('You are awesome! All tests are done! '
+          'Go Check it!')
