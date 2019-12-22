fe46113faa0ce4d0579f29a4899e75da2e5707ac
eldar-dniaprouski <eldar.dnep@gmail.com>
2019-12-22 18:34:54 +0300

    Homework 3 pary_elementov

--- src/Homework_03/04_pary_elementov.py
+++ src/Homework_03/04_pary_elementov.py
@@ -7,3 +7,13 @@
 Важно: 1 1 1 - это 3 пары, 1 1 1 1 - это 6 пар
 """
 
+num_string = '1 1 1 1'
+num_list = num_string.split()
+num_pair = 0
+check_list = num_list[:]
+for elem in num_list:
+    check_list.remove(elem)
+    for item in check_list:
+        if item == elem:
+            num_pair += 1
+print(num_pair)
