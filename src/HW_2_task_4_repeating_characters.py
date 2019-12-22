8fca13a15e26b67f76b41f05c628bb441e547432
Dzmitry89 <bachkovskiydv@gmail.com>
2019-12-23 00:06:47 +0300

    comments design correction

--- src/HW_2_task_4_repeating_characters.py
+++ src/HW_2_task_4_repeating_characters.py
@@ -5,12 +5,17 @@
 string = "abc cde def"
 string = string.replace(" ", "")  # удаляем пробелы
 char_index = 0
-while char_index < len(string) - 1:  # перебираем все элементы, кроме последнего
-    new_index = char_index + 1  # доп. счетчик для прохода по всем след. эл-там
-    while new_index < len(string):  # просматриваем все эл-ты, стоящие после текущего
+while char_index < len(string) - 1:  # перебираем все элементы,/
+    # кроме последнего
+    new_index = char_index + 1  # доп. счетчик для прохода по
+    # всем след. эл-там
+    while new_index < len(string):  # просматриваем все эл-ты,
+        # стоящие после текущего
         if string[char_index] == string[new_index]:  # если символы совпали
-            string = string[:new_index] + string[new_index + 1:]  # подрезаем строку
+            string = string[:new_index] + string[new_index + 1:]  # подрезаем
+            # строку
         else:
-            new_index += 1  # если нет - переходим к следующему сравниваему элементу
+            new_index += 1  # если нет - переходим к следующему сравниваему
+            # элементу
     char_index += 1  # переходим к след. уникальному символу
 print(string)
