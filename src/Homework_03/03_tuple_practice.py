4a89c42dc3127c6c40038c74de42e153e9e192cb
eldar-dniaprouski <eldar.dnep@gmail.com>
2019-12-22 16:05:57 +0300

    Homework 3 tuple_practice

--- src/Homework_03/03_tuple_practice.py
+++ src/Homework_03/03_tuple_practice.py
@@ -2,10 +2,21 @@
 Создайте список ['a', 'b', 'c'] и сделайте из него кортеж.
 Создайте кортеж ('a', 'b', 'c'), И сделайте из него список
 Сделайте следующие присвоения одной строкой a = 'a', b=2, c=’python’.
-Создайте кортеж из одного элемента,
-чтобы при итерировании по этому элементы
+Создайте кортеж из одного элемента, чтобы при итерировании по этому элементы
 последовательно выводились значения 1, 2, 3.
 Убедитесь что len() исходного кортежа возвращает 1.
 """
 
-
+my_first_list = [a for a in 'abc']
+print(my_first_list)
+my_first_tuple = tuple(my_first_list)
+print(my_first_tuple)
+my_second_tuple = tuple(b for b in 'abc')
+print(my_second_tuple)
+my_second_list = list(my_second_tuple)
+print(my_second_list)
+a, b, c = 'a', 2, 'python'
+my_one_tuple = ('123', )
+for elem in my_one_tuple[0]:
+    print(elem, end=' ')
+print('\nLength of tuple =', len(my_one_tuple))
