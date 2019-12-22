8c2000a5a9e6cffc35cce33c7fb387a2c76db1e0
eldar-dniaprouski <eldar.dnep@gmail.com>
2019-12-22 14:51:47 +0300

    Homework 3 FizzBuzz

--- src/Homework_03/01_FizzBuzz.py
+++ src/Homework_03/01_FizzBuzz.py
@@ -4,3 +4,12 @@
 вместо чисел кратный 5 пишет Buzz,
 а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
 """
+for num in range(1, 101):
+    if not num % 3 and not num % 5:
+        print('FizzBuzz')
+    elif not num % 3:
+        print('Fizz')
+    elif not num % 5:
+        print('Buzz')
+    else:
+        print(num)
