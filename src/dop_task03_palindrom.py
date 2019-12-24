10596146f428075576f4863a9fd487a55f6d2727
vorongn <vorongn@gmail.com>
2019-12-24 09:35:46 +0300

    fix

--- src/dop_task03_palindrom.py
+++ src/dop_task03_palindrom.py
@@ -1,14 +1,14 @@
 # палиндром
 # палиндром - одинаково читающееся в обоих направлениях.
 num = int(input('Введите число: '))
-tempNum = num
+temp_num = num
 num2 = 0
 if num < 0:
     print(num, ' - не палиндром')
 else:
-    while tempNum > 0:
-        num2 = num2 * 10 + tempNum % 10
-        tempNum = tempNum // 10
+    while temp_num:
+        num2 = num2 * 10 + temp_num % 10
+        temp_num //= 10
     else:
         if num == num2:
             print(num, ' - палинром')
--- src/task03_samoe_dlinnoe.py
+++ src/task03_samoe_dlinnoe.py
@@ -4,14 +4,16 @@
 """
 
 
-my_string = '1 22 333 4444 55555 55555, 333. 666666.....  333'
-my_string1 = my_string.replace(",", " ")
-my_string1 = my_string1.replace(".", " ")
-my_string2 = my_string1.split()
+my_string = '1 22 333 4444 55555 55555, 333.! 666666.....  333???????'
+prep = ',.?!'
+for simbol in my_string:
+    if simbol in prep:
+        my_string = my_string.replace(simbol, " ")
+my_string2 = my_string.split()
 max_string_count = 0
 max_string = ''
-for i in my_string2:
-    if max_string_count < len(i):
-        max_string_count = len(i)
-        max_string = i
+for word in my_string2:
+    if max_string_count < len(word):
+        max_string_count = len(word)
+        max_string = word
 print('маХ длинна - ', max_string_count, 'слово - ', max_string)
