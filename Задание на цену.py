d1850533ada6e63836c538cab223fb1dba73a07e
FedorPolikarpov <58562211+FedorPolikarpov@users.noreply.github.com>
2019-12-25 15:06:34 +0300

    Update Задание на цену.py

--- Задание на цену.py
+++ Задание на цену.py
@@ -1,43 +1,36 @@
 # Посчитать стоимость
-m = int(input('Введите стоимость вещи (руб):'))
-n = int(input('Введите стоимость вещи (коп):'))
-l = int(input('Введите колличество товара, которое хотите купить:'))
-# Узнаем, сколько руб и коп стоит вещь
-cost1 = m * l
-cost2 = n * l
+rub_input = int(input('Введите стоимость вещи (руб):'))
+cop_input = int(input('Введите стоимость вещи (коп):'))
+quant = int(input('Введите колличество товара, которое хотите купить:'))
+cost1 = rub_input * quant
+cost2 = cop_input * quant
 # Если копеек больше 100, остаток переведем в рубли
 while cost2 >= 100:
     cost3 = cost2 // 100
     cost1 = cost1 + cost3
     cost2 = cost2 - (cost3 * 100)
-# Сделаем красивый вывод ответа,
-# что бы не было ( "стоит 22 рублей" или "стоит 1 копеек")
-# Для этого узнаем, на какую цифру заканчивается число рублей и копеек
-# для рублей:
 s = str(cost1)
-costrub = int(s[-1])
+cost_rub = int(s[-1])
 # для копеек используем другой способ, без приведения к строке, но интереснее:
-costcop1, costcop2 = cost2 / 10, cost2 // 10
-costcop = int((costcop1 - costcop2) * 10)
+cost_cop1, cost_cop2 = cost2 / 10, cost2 // 10
+cost_cop = int((cost_cop1 - cost_cop2) * 10)
 # делаем красивый output:
-# сначала рубли
-if costrub == 0 or costrub == 5 or costrub == 6 or cost1 == 11:
+if cost_rub == 0 or cost_rub == 5 or cost_rub == 6 or cost1 == 11:
     print('Общая цена: {0} рублей'.format(cost1), end="")
 # создаю такой же output для 7, 8 и 9
 # потому что если их засунуть в if, будет слишком длинная строка
-elif costrub == 7 or costrub == 8 or costrub == 9:
+elif cost_rub == 7 or cost_rub == 8 or cost_rub == 9:
     print('Общая цена: {0} рублей'.format(cost1), end="")
-elif costrub == 2 or costrub == 3 or costrub == 4:
+elif cost_rub == 2 or cost_rub == 3 or cost_rub == 4:
     print('Общая цена: {0} рубля'.format(cost1), end="")
-elif costrub == 1:
+elif cost_rub == 1:
     print('Общая цена: {0} рубль'.format(cost1), end="")
 # теперь копейки
-if costcop == 2 or costcop == 3 or costcop == 4:
+if cost_cop == 2 or cost_cop == 3 or cost_cop == 4:
     print(' {0} копейки'.format(cost2))
-elif costcop == 5 or costcop == 6 or cost2 == 11:
+elif cost_cop == 5 or cost_cop == 0 or cost_cop == 6 or cost2 == 11:
     print(' {0} копеек'.format(cost2))
-elif costcop == 7 or costcop == 8 or costcop == 9:
+elif cost_cop == 7 or cost_cop == 8 or cost_cop == 9:
     print(' {0} копеек'.format(cost2))
-elif costcop == 1:
+elif cost_cop == 1:
     print(' {0} копейку'.format(cost2))
-# получилось объемно, зато красиво )
