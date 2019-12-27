2f1f52608322e18de5f7af6d92b0af57c5016d51
vorongn <vorongn@gmail.com>
2019-12-27 15:23:33 +0300

    hw03

--- src/hw03_task07_chokoladka.py
+++ src/hw03_task07_chokoladka.py
@@ -16,14 +16,16 @@ shoko02 = int(input('размер шоколадки N:'))
 doley = int(input('k - долек:'))
 print(int(shoko01), int(shoko02), doley)
 if doley <= shoko01 * shoko02 and shoko01 > 0 and shoko02 > 0:
-    if (doley % shoko01 == 0 and doley >= shoko01) or (doley % shoko02 == 0 and shoko01 <= doley):
+    if (doley % shoko01 == 0 and doley >= shoko01) \
+            or (doley % shoko02 == 0 and shoko01 <= doley):
         print('можно отломить', doley, 'долей за раз')
     else:
         print('за раз НЕ отломаешь', doley, 'долей')
 if doley == shoko01 * shoko02:
     print('От шоколадки МОЖНО отломить ровно ', doley, 'одиночных долей')
 else:
-    print('НЕЛЬЗЯ отломить ровно', doley, 'одиночных  долей! Их ровно', shoko01 * shoko02)
+    print('НЕЛЬЗЯ отломить ровно', doley, 'одиночных  долей! '
+                                          'Их ровно', shoko01 * shoko02)
 
 razlomi = int(input('разломов:'))
 if doley == razlomi and doley < shoko01 * shoko02:
