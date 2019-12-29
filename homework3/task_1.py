a25197a778b9ee217887060580ac11caab1cba77
Nicolay <nicolay.krischenovich@gmail.com>
2019-12-29 17:44:48 +0300

    Fix code style

--- homework3/task_1.py
+++ homework3/task_1.py
@@ -2,7 +2,7 @@
 # Напишите программу, которая печатает цифры от 1 до 100, но вместо чисел,
 # кратных 3 пишет Fizz, вместо чисел кратный 5 пишет Buzz, а вместо чисел
 # одновременно кратных и 3 и 5 - FizzBuzz
-# lst = [element for element in range(100) element = 'Fizz' if element % 3 == 0]
+
 lst = []
 for element in range(1, 100):
     if element % 3 == 0 and element % 5 != 0:
