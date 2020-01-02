38548493820f1b1f5c5d453399efa4fbe3702985
renat33 <58603739+renat33@users.noreply.github.com>
2020-01-02 21:42:47 +0300

    Delete cost.py

--- cost.py
+++
@@ -1,7 +0,0 @@
-m = int(input("Введите кол-во рублей\n"))
-n = int(input("Введите кол-во копеек\n"))
-numGoods = int(input("Введите кол-во покупаемого товара\n"))
-costn = n * numGoods / 100
-cost = costn + (m * numGoods)
-print("Общая цена составляет", cost, "рублей")
-print("Или", cost // 1, "руб.", round((cost - (cost // 1)) * 100), "коп.")
