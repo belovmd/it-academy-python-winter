72c5259c532b1eb098ef942002dc1159abfda706
ZhukAlexander90 <earl.j.hickey0@gmail.com>
2020-01-08 19:23:58 +0300

    fix task_2

--- Homework3_ZhukAlexander/task2_list_practice.py
+++ Homework3_ZhukAlexander/task2_list_practice.py
@@ -13,7 +13,7 @@ import copy
 first_lst = [first + second for first in "ab" for second in "bcd"]
 print(first_lst)
 print(first_lst[::2])
-second_lst = [str(num) + letter for num in range(1, 5) for letter in "a"]
+second_lst = [num + letter for num in "1234" for letter in "a"]
 print(second_lst)
 print(second_lst.pop(1))
 third_lst = copy.copy(second_lst)
