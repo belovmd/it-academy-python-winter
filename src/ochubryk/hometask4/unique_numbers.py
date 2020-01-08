a1f0b6a4899f43a185407a62e5093acb642ac39b
Ольга Чубрик <olia9867@yandex.ru>
2020-01-08 23:28:16 +0300

    Homework4: Olga Chubryk

---
+++ src/ochubryk/hometask4/unique_numbers.py
@@ -0,0 +1,21 @@
+# Даны два списка чисел. Посчитайте, сколько
+# чисел входит только в один из этих списков
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+b = [11, 12, 13, 3, 4, 5, 15, 16, 7]
+
+# print(len(set.difference(set(a), set(b))))
+print('Числа, уникальные для списка а: ' + str(set.difference(set(a), set(b))))
+print('Количество чисел, уникальных для списка а: ' +
+      str(len(set.difference(set(a), set(b)))))
+
+print('Числа, уникальные для списка b: ' + str(set.difference(set(b), set(a))))
+print('Количество чисел, уникальных для списка b: ' +
+      str(len(set.difference(set(b), set(a)))))
+
+print('Множество из элементов, встречающихся в одном множестве,'
+      ' но не встречающиеся в обоих: ' +
+      str(set.symmetric_difference(set(a), set(b))))
+print('Количество элементов, встречающихся в одном множестве,'
+      ' но не встречающиеся в обоих: ' +
+      str(len(set.symmetric_difference(set(a), set(b)))))
