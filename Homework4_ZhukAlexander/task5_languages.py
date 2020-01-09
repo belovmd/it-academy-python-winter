0ec8bf708f4b17d1a9308ed94b4ea269432aad7b
ZhukAlexander90 <earl.j.hickey0@gmail.com>
2020-01-09 21:46:23 +0300

    Homework_4 complete

--- Homework4_ZhukAlexander/task5_languages.py
+++ Homework4_ZhukAlexander/task5_languages.py
@@ -1,7 +1,7 @@
 """
 Каждый из N школьников некоторой школы знает Mi языков.
-Определите, какие языки знают все школьники и языки,
-которые знает хотя бы один из школьников.
+Определите, какие языки знают все школьники
+и языки, которые знает хотя бы один из школьников.
 
 Входные данные
 Первая строка входных данных содержит количество школьников N.
@@ -22,3 +22,14 @@ Italian
 French
 
 """
+
+student_list = [{input("Язык школьника: ") for qnt_languages in
+                range(int(input("Количество языков: ")))}
+                for qnt_students in range(int(input("Количество "
+                                                    "школьников: ")))]
+# print(student_list)
+all_know = set.intersection(*student_list)
+somebody_know = set.union(*student_list)
+print("Языки, которые знают все: ", len(all_know), ":", all_know)
+print("Языки, которые знает хотя "
+      "бы один из школьников: ", len(somebody_know), ":", somebody_know)
--- Homework4_ZhukAlexander/task7_evklid.py
+++ Homework4_ZhukAlexander/task7_evklid.py
@@ -12,6 +12,7 @@ while first_num != second_num:          # нахождение НОД вычит
     else:
         second_num -= first_num
 print(first_num)
+
 first_num = int(input("Первое число: "))
 second_num = int(input("Второе число: "))
 while first_num != 0 and second_num != 0:       # нахождение НОД делением
