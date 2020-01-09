4b5c10c09b1f0d57d99d4368c7ff2713107522e4
renat33 <58603739+renat33@users.noreply.github.com>
2020-01-09 20:04:46 +0300

    Update zadacha1.py

--- zadacha1.py
+++ zadacha1.py
@@ -3,7 +3,7 @@
 # вместо чисел кратный 5 пишет Buzz,
 # а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
 for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
+    if i % 15 == 0:
         print("FizzBuzz")
     elif i % 3 == 0:
         print("Fizz")
