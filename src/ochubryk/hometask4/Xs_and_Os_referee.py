46e589148b2e2bd1cae0ac97281916167bebab55
Ольга Чубрик <olia9867@yandex.ru>
2020-01-13 22:17:57 +0300

    Fixed code style

--- src/ochubryk/hometask4/Xs_and_Os_referee.py
+++ src/ochubryk/hometask4/Xs_and_Os_referee.py
@@ -20,13 +20,17 @@ from typing import List
 
 def checkio(game_result: List[str]) -> str:
     for i in range(3):
-        if game_result[i][0] == game_result[i][1] == game_result[i][2] and game_result[i][0] != ".":
+        if game_result[i][0] == game_result[i][1] == game_result[i][2]\
+                and game_result[i][0] != ".":
             return game_result[i][0]
-        elif game_result[0][i] == game_result[1][i] == game_result[2][i] and game_result[0][i] != ".":
+        elif game_result[0][i] == game_result[1][i] == game_result[2][i]\
+                and game_result[0][i] != ".":
             return game_result[0][i]
-        elif game_result[0][0] == game_result[1][1] == game_result[2][2] and game_result[0][0] != ".":
+        elif game_result[0][0] == game_result[1][1] == game_result[2][2]\
+                and game_result[0][0] != ".":
             return game_result[1][1]
-        elif game_result[0][2] == game_result[1][1] == game_result[2][0] and game_result[0][2] != ".":
+        elif game_result[0][2] == game_result[1][1] == game_result[2][0]\
+                and game_result[0][2] != ".":
             return game_result[1][1]
     return 'D'
 
--- src/ochubryk/hometask4/common_words.py
+++ src/ochubryk/hometask4/common_words.py
@@ -20,4 +20,5 @@ def checkio(first, second):
 if __name__ == '__main__':
     assert checkio("hello,world", "hello,earth") == "hello", "Hello"
     assert checkio("one,two,three", "four,five,six") == "", "Too different"
-    assert checkio("one,two,three", "four,five,one,two,six,three") == "one,three,two", "1 2 3"
+    assert checkio("one,two,three",
+                   "four,five,one,two,six,three") == "one,three,two", "1 2 3"
