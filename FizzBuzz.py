0cf2093c04eff4420d02bfda9943c92f188a04b2
FedorPolikarpov <58562211+FedorPolikarpov@users.noreply.github.com>
2020-01-13 18:45:11 +0300

    Update and rename FzzBuzz.py to FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,7 +2,7 @@
 # но вместо чисел, кратных 3 пишет Fizz, вместо чисел кратных 5 пишет
 # Buzz, а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
 for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
+    if i % 15 == 0:
         print('FizzBuzz')
     elif i % 5 == 0:
         print('Buzz')
