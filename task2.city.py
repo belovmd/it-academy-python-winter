e18c154af70db896f3ef0c3b5206091562b4f8e0
Igor Bohdan <igor3407.b@gmail.com>
2020-01-14 11:42:04 +0300

    homework 4

---
+++ task2.city.py
@@ -0,0 +1,29 @@
+# Дан список стран и городов каждой страны.
+# Затем даны названия городов. Для каждого
+# города укажите, в какой стране он находится.
+#
+# исходные данные:
+# 2
+# Russia Moscow Petersburg Novgorod Kaluga
+# Ukraine Kiev Donetsk Odessa
+# 3
+# Odessa
+# Moscow
+# Novgorod
+# #########
+# Создаем словарь с ключом - страна, значение - города(список),
+# получаем список городов для проверки, пробегаемся по нему,
+# сверяем значение словаря со списком для проверки, если
+# находим печатаем ключ.
+my_dict = {}
+for i in range(int(input())):
+	country, *city = input().split()
+	my_dict[country] = city  # создаем словарь
+list_check = []
+for j in range(int(input())):
+	city = input()
+	list_check.append(city)  # сщздаем список для проверки
+for u in list_check:
+	for key, value in my_dict.items():
+		if u in value:
+			print(key)
---
+++ task4.different.numbers.py
@@ -0,0 +1,12 @@
+# Даны два списка чисел.
+# Посчитайте, сколько различных
+# чисел входит только в один из этих списков
+
+a = set(input().split())
+b = set(input().split())
+count = 0
+d = []
+for i in a:
+	if i not in b:
+		d.append(i)
+print(len(d))
