a862d324109ed128bdcf0ebda27f794e1f147cd3
Dmitryikot <dmitryi.kot@gmai.com>
2020-01-16 18:31:43 +0300

    Fix Homework4

--- src/Homework4.py
+++ src/Homework4.py
@@ -105,7 +105,7 @@ French
 stud = [
     {input('lang: ')
      for j in range(int(input('count lang: ')))}
-      for i in range(int(input('stud: ')))]
+       for i in range(int(input('stud: ')))]
 known_everyone, known_someone = set.intersection(*stud), set.union(*stud)
 print(len(known_everyone), *sorted(known_everyone), sep='\n')
 print(len(known_someone), *sorted(known_someone), sep='\n')
