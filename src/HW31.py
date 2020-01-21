dcbb9283b46b40d4dd35839517a106a387b1355e
Ioann87 <58548550+Ioann87@users.noreply.github.com>
2020-01-21 16:46:57 +0300

    fixed hw3

---
+++ src/HW31.py
@@ -0,0 +1,11 @@
+# Напишите программу, которая печатает цифры от 1 до 100, но вместо чисел, кратных 3 пишет Fizz,
+# вместо чисел кратный 5 пишет Buzz, а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
+for num in range(1, 101):
+    if num % 15 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("buzz")
+    elif num % 5 == 0:
+        print("fizz")
+    else:
+        print(num)
