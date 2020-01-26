0e0bb79b19a9f3937f8b1f110651adca66abf4b2
chechuhae <58543706+chechuhae@users.noreply.github.com>
2020-01-26 17:10:52 +0300

    Just save

---
+++ src/FunctionsP/Runner.py
@@ -0,0 +1,14 @@
+from FunctionsP import FizzBuzz
+
+
+FizzBuzz()
+"""def runner(*args):
+
+    if not args:
+        for el in dir(Functi):
+            print("Hello")
+    elif args:
+        for el in args:
+            args
+    else:
+        print("Функция не найдена")"""
