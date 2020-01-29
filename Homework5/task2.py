21ca6eb5a6df02c99f59c405df7c4481060d6568
alex-shef <alexei.shef@gmail.com>
2020-01-30 02:13:00 +0300

    Corrections

--- Homework5/task2.py
+++ Homework5/task2.py
@@ -5,7 +5,7 @@
 def data_to_file(func):
     def wrapper():
         with open('file.txt', 'a') as f:
-            f.write(func()+'\n')
+            f.write(func() + '\n')
     return wrapper
 
 
@@ -15,14 +15,14 @@ def data_to_file(func):
 def gcd():
     number1 = int(input('Enter 1st natural number: '))
     number2 = int(input('Enter 2nd natural number: '))
-    data = str(number1)+' '+str(number2)   # Для записи в файл
+    data = str(number1) + ' ' + str(number2)   # Для записи в файл
     while number1 != number2:
         if number1 > number2:
             number1 = number1 - number2
         else:
             number2 = number2 - number1
     print('GCD =', number1)  # greatest common divisor
-    data = data+' GCD = '+str(number1)
+    data = data + ' GCD = ' + str(number1)
     return data
 
 
