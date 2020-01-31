0f1e247f0e838ea735fe529736033aefc0e10805
chechuhae <58543706+chechuhae@users.noreply.github.com>
2020-01-31 23:41:29 +0300

    HW without 5th task

---
+++ src/FunctionsP/DegreeFour.py
@@ -0,0 +1,26 @@
+"""
+4.	Написать программу которая находит ближайшую степень двойки к введенному
+числу. 10(8), 20(16), 1(1)
+"""
+
+
+def DegreeFour(a=None):
+    if not a:
+        a = int(input())
+    a_1 = 2
+    a_2 = 2
+    while a_1 * 2 < a and a_1 * 2 != a:
+        a_1 *= 2
+    while a_2 < a and a_2 != a:
+        a_2 *= 2
+    if a == 1:
+        return print(a)
+    elif a - a_1 < a_2 - a:
+        return print(a_1)
+    elif a_2 - a < a - a_1:
+        return print(a_2)
+    else:
+        return print(a_1)
+
+
+DegreeFour()
--- src/FunctionsP/Top.py
+++ src/FunctionsP/Top.py
@@ -6,9 +6,72 @@
 гистограмма рейтингов, years.txt – гистограмма годов.
 """
 
-try:
-    f = open("data5/rating.list", "r")
-    f.close()
-except IOError:
-    print("ERROR")
 
+def Reader(a="ratings.list"):
+    try:
+        site = open(a, "r")
+        site.close()
+    except IOError:
+        return(print("ERROR"))
+
+
+def Filer(a="ratings.list"):
+    site = open(a, "r")
+    top = open("top250_movies.txt", "w")
+    counter = 1
+    for line in site:
+        counter += 1
+        if 29 < counter < 269:
+            new_line = line.split()
+            new_line = " ".join(new_line[3:-1:])
+            top.write(str(new_line))
+            top.write("\n")
+    top.close()
+    site.close()
+
+    site = open(a, "r")
+    ratings = open("ratings.txt", "w")
+    counter = 0
+    lst_rate = []
+    dct_1 = {}
+    for line in site:
+        counter += 1
+        if 29 < counter < 269:
+            new_line = line.split()
+            new_line = " ".join(new_line[2:3:])
+            lst_rate.append(new_line)
+    for el in lst_rate:
+        dct_1[el] = dct_1.get(el, 0) + 1
+    for el in dct_1.keys():
+        text_1 = str(str(el)) + " " + "*" * dct_1.get(el) + " "\
+                 + str(dct_1[el])
+        ratings.write(text_1 + "\n")
+    ratings.close()
+    site.close()
+
+    site = open(a, "r")
+    years = open("years.txt", "w")
+    counter = 0
+    lst_years = []
+    dct_2 = {}
+    for line in site:
+        counter += 1
+        if 29 < counter < 269:
+            new_line = line.split()
+            new_line = "".join(new_line[-1::])
+            new_line = (new_line.replace("(", "")).replace(")", "")
+            lst_years.append(new_line)
+    for el in lst_years:
+        dct_2[el] = dct_2.get(el, 0) + 1
+    print(dct_2)
+    for el in dct_2.keys():
+        text_2 = str(str(el) + " " + "*" * dct_2.get(el) + " "
+                     + str(dct_2[el]))
+        years.write(text_2 + "\n")
+
+    years.close()
+    site.close()
+
+
+Reader()
+Filer()
