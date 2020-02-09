46f5689d75c9f84fc6ae650fd5f6f8d34d88840c
Nicolay <nicolay.krischenovich@gmail.com>
2020-02-09 10:35:52 +0300

    fix flake8

--- Homework5/task_1.py
+++ Homework5/task_1.py
@@ -4,12 +4,65 @@
 # runner_b(‘func_name’) – вызывается только функцию func_name.
 # runner_c(‘func’, ‘func1’...) - вызывает все переданные функции
 
-from my_functions import *
 
-'''list of functions :
-gen_dct() intersec(), semetric(), lenguagies(),
- words(), evklid()
-'''
+def gen_dct():
+    dct = {key: key ** 3 for key in range(1, 21)}
+    print(dct)
+    return dct
+
+
+def intersec():
+    lst1 = set([i for i in
+                (input('Please, enter the list of numbers '
+                       'through space: ')).split()])
+    # print(lst1)
+
+    lst2 = set([i for i in
+                (input('Please, enter the list of numbers '
+                       'through space: ')).split()])
+    # print(lst2)
+
+    print(len(lst1.intersection(lst2)))
+    # print(lst1 & lst2)
+    return len(lst1.intersection(lst2))
+
+
+def semetric():
+    lst1 = set([i for i in
+                (input('Please, enter the list of numbers '
+                       'through space: ')).split()])
+    # print(lst1)
+
+    lst2 = set([i for i in
+                (input('Please, enter the list of numbers '
+                       'through space: ')).split()])
+    # print(lst2)
+
+    print(len(lst1.symmetric_difference(lst2)))
+    # print(lst1.symmetric_difference(lst2))
+    return len(lst1.symmetric_difference(lst2))
+
+
+def words():
+    new_lst = []
+    text = input(str(" Type some text: "))
+    lst = text.split()
+    for word in lst:
+        word.strip()
+        new_lst.append(word)
+    print(len(new_lst))
+
+
+def evklid():
+    a = int(input("Type first number: "))
+    b = int(input("Type second number: "))
+    if a < b:
+        a, b = b, a
+    ost = a % b
+    while ost != 0:
+        ost = a % b
+        a, b, = b, ost
+    print(a)
 
 
 # print(globals())
@@ -26,6 +79,6 @@ def runner(*func):
             functions[i]()
 
 
-runner()
-# runner('num_intersect')
-# runner('evklid', 'gen_dct')
+# runner()
+# runner()
+runner('evklid', 'gen_dct', 'semetric')
--- Homework5/task_2.py
+++ Homework5/task_2.py
@@ -4,6 +4,7 @@
 
 def dec(func):
     s = []
+
     def wrapper(*args):
         nonlocal s
         s.append(func(*args))
@@ -11,9 +12,11 @@ def dec(func):
         return func
     return wrapper
 
+
 @dec
 def summator(a, b):
     return a + b
+
 
 summator(1, 2)
 summator(2, 2)
