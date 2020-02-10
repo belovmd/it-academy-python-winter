6b650894c2a4f99c570cc10970b982e772a13d67
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2020-02-10 15:42:46 +0300

    Update exercise4.py

--- src/exercise4.py
+++ src/exercise4.py
@@ -1,5 +1,4 @@
 # Даны два списка чисел. Посчитайте, сколько чисел
 # содержится одновременно как в первом списке, так и во втором
 
-print(len(set.intersection(set(input("Please, enter numbers: ")), 
-                           set(input("Please, enter numbers: ")))))
+print(len(set.intersection(set(input(" ")), set(input(" ")))))
