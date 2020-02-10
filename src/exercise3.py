b6ea838911ef13f8fa57f68ae58d89310a1cad5e
maryiaLichko <51047876+maryiaLichko@users.noreply.github.com>
2020-02-10 15:36:08 +0300

    Update exercise3.py

--- src/exercise3.py
+++ src/exercise3.py
@@ -1,4 +1,5 @@
 # Даны два списка чисел. Посчитайте, сколько
 # чисел входит только в один из этих списков
 
-print(len(set(input("Please, enter some numbers").split()) & set(input("Please, enter some numbers").split())))
+print(len(set(input("Please, enter some numbers").split()) 
+& set(input("Please, enter some numbers").split())))
