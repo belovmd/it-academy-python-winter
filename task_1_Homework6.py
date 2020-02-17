215f4dc697a82c99bb07d415af57697edd25208c
Igor Bohdan <igor3407.b@gmail.com>
2020-02-17 11:07:17 +0300

    Homework6, task 1, 2.

---
+++ task_1_Homework6.py
@@ -0,0 +1,66 @@
+﻿""" Создайте  модель из жизни.
+    Это может быть бронирование комнаты в отеле,
+    покупка билета в транспортной компании, или простая РПГ.
+    Создайте несколько объектов классов, которые описывают ситуацию
+    Объекты должны содержать как атрибуты так и методы класса
+    для симуляции различных действий.
+    Программа должна инстанцировать объекты и эмулировать
+    какую-либо ситуацию - вызывать методы, взаимодействие объектов и т.д.
+"""
+
+
+class AddressBookEntry:
+    version = 1.0
+
+    def __init__(self, name, phone):
+        self.name = name
+        self.phone = phone
+
+    def __repr__(self):
+        return ' [Address Book Entry: %s, %s ] ' % (self.name, self.phone)
+
+    def updatePhone(self, phone):
+        self.phone = phone
+
+
+class EmployeeAddressBookEntry(AddressBookEntry):
+    def __init__(self, name, phone, id, social):
+        AddressBookEntry.__init__(self, name, phone)
+        self.empid = id
+        self.ssn = social
+
+
+class CustomerAddressBookEntry(AddressBookEntry):
+    def __init__(self, name, phone, order, date):  # Переопределить конструктор
+        self.order = order
+        self.date = date
+        AddressBookEntry.__init__(self, name, phone)  # Выполнить исходный c ’mgr1
+
+    def updateOrder(self, order):
+        self.order = []
+        self.order = self.order.extend(order)
+
+    def updateDate(self, date):
+        self.date = []
+        self.date = self.date.extend(date)
+
+    def __repr__(self):
+        return ' [Customer Address Book Entry: %s, %s, %s, %s ] ' % (self.name,
+                                                                     self.phone,
+                                                                     self.order,
+                                                                     self.date)
+
+
+robert = AddressBookEntry('Staf', '55-789-54645-55')
+
+print(robert.name, robert.phone)
+
+mitchel = CustomerAddressBookEntry('Makl Mitchel',
+                                   +375447496999,
+                                   'Order № 25',
+                                   '25.01.2020')
+
+print(mitchel)
+# "Магический" метод __repr__ в данном случае иллюстрирует
+# проявление полиморфизма.
+print(robert)
---
+++ task_2_Homework6.py
@@ -0,0 +1,40 @@
+﻿""" Создайте декоратор,
+    который вызывает задекорированную функцию
+    пока она не выполнится без исключений
+    (но не более n раз - параметр декоратора).
+    Если превышено количество попыток,
+    должно быть возбуждено исключение типа TooManyErrors
+
+"""
+
+import random
+import functools
+
+
+def retry(max_tries):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for n in range(1, max_tries + 1):
+                try:
+                    return func(*args, **kwargs)
+                except Exception:
+                    if n == max_tries:
+                        raise
+
+        return wrapper
+
+    return decorator
+
+
+@retry(max_tries=10)
+def do_something_unreliable():
+    """Do something unreliably."""
+    if random.randint(0, 10) > 1:
+        raise IOError("TooManyErrors")
+    else:
+        return "Awesome"
+
+
+n = do_something_unreliable()
+print(n)
