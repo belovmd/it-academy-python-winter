fcc1e6a9dc0be738c59ad9f3b75a4bdcba747f98
renat33 <58603739+renat33@users.noreply.github.com>
2020-02-23 21:03:03 +0300

    Update hw4_Zadacha7.py

--- hw4_Zadacha7.py
+++ hw4_Zadacha7.py
@@ -3,10 +3,10 @@
 
 num1 = int(input())
 num2 = int(input())
-while num1 != 0 and num2 != 0:
+while num1 != num2:
     if num1 > num2:
-        num1 = num1 % num2
+        num1 = num1 - num2
     else:
-        num2 = num2 % num1
+        num2 = num2 - num1
 
-print(num1 + num2)
+print(num1)
