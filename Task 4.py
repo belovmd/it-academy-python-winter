c173ff259ee1c1ccd0c83674889ace03ae6ece07
FedorPolikarpov <58562211+FedorPolikarpov@users.noreply.github.com>
2020-02-27 15:09:30 +0300

    Update Task 4.py

--- Task 4.py
+++ Task 4.py
@@ -2,17 +2,4 @@
 # входит только в один из этих списков
 user_lst1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 300, 11, 11]
 user_lst2 = [100, 2, 200, 3, 300, 4, 500, 9, 100, 5, 5, 444]
-output_lst1 = []
-output_lst2 = []
-for i in range(len(user_lst1)):
-    if user_lst1[i] in output_lst1:
-        continue
-    else:
-        output_lst1.append(user_lst1[i])
-print(len(output_lst1))
-for i in range(len(user_lst2)):
-    if user_lst2[i] in output_lst2:
-        continue
-    else:
-        output_lst2.append(user_lst2[i])
-print(len(output_lst2))
+print(len(set(user_lst1) ^ set(user_lst2)))
