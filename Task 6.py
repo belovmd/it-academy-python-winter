0393b0c66c3265282cbcb710218f9b35ac5ce1fd
FedorPolikarpov <58562211+FedorPolikarpov@users.noreply.github.com>
2020-02-27 15:09:52 +0300

    Update Task 6.py

--- Task 6.py
+++ Task 6.py
@@ -2,11 +2,6 @@
 # непробельных символов идущих подряд, слова разделены одним или большим
 # числом пробелов или символами конца строки. Определите, сколько различных
 # слов содержится в этом тексте.
-user_input = input().split()
-lst_output = []
-for i in range(len(user_input)):
-    if user_input[i] in lst_output:
-        continue
-    else:
-        lst_output.append(user_input[i])
-print(len(lst_output))
+user_input = [el for el in input().lower().split()]
+sett = set(user_input)
+print(len(sett))
