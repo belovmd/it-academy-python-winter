29e78a44070390aa2cce4bda85dfdaf72ff66908
FedorPolikarpov <58562211+FedorPolikarpov@users.noreply.github.com>
2020-02-27 15:09:11 +0300

    Update Task 3.py

--- Task 3.py
+++ Task 3.py
@@ -2,11 +2,4 @@
 # содержится одновременно как в первом списке, так и во втором.
 user_lst1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 300, 11]
 user_lst2 = [100, 2, 200, 3, 300, 4, 500, 9, 100, 5, 5, 444]
-sum_lst = user_lst1 + user_lst2
-output_lst = []
-for i in range(len(sum_lst)):
-    if sum_lst[i] in output_lst:
-        continue
-    else:
-        output_lst.append(sum_lst[i])
-print(len(output_lst))
+print(len((set(user_lst1).intersection(set(user_lst2)))))
