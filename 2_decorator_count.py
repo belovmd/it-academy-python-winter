e6dca87a5be5896a720b6ca40a63bb1ee20c2b40
Igor Bohdan <igor3407.b@gmail.com>
2020-03-23 16:30:54 +0300

    fixed in accordance with comments received

--- 2_decorator_count.py
+++ 2_decorator_count.py
@@ -2,30 +2,18 @@
 который хранит результаты вызовы функции
 (за все время вызовов, не только текущий запуск программы)"""
 
-
 def dec(fun):
-    count = 0
-    my_file = open("snake.txt", "a+")
-    my_file.write('1')
-    my_file.close()
-    my_file = open('snake.txt')
-    my_str = my_file.read()
-    print(my_str)
-    my_file.close()
-
-    for i in my_str:
-        count += int(i)
-    print('функция вызывалась ', count, ' раз(а)')
-
     def wrapper():
-        return fun()
-
+        count = 0
+        my_file = open("snake.txt", "a+")
+        my_file.write('1')
+        my_file = open('snake.txt')
+        my_str = my_file.read()
+        my_file.close()
+        for i in my_str:
+            count += int(i)
+        print('функция вызывалась ', count, ' раз(а)')
+        return fun
     return wrapper
 
 
-@dec
-def func():
-    pass
-
-
-func()
