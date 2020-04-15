06b1660d32a3a59da0cec094d6076e7d0497430a
Danila Zhogol <danzhog@gmail.com>
2020-04-15 20:24:44 +0300

    Initial commit

---
+++ src/homework5/1_runner.py
@@ -0,0 +1,31 @@
+# Оформите решение задач из прошлых домашних работ в функции.
+# Напишите функцию runner.
+# runner() – все фукнции вызываются по очереди
+# runner(‘func_name’) – вызывается только функцию func_name
+# runner(‘func’, ‘func1’...) - вызывает все переданные функции
+
+import my_func
+import inspect
+
+
+def runner(*args):
+    if not args:
+        func_list = [el for el in dir(my_func) if not el.startswith('__')]
+    else:
+        func_list = [*args]
+
+    for func in func_list:
+        try:
+            func = getattr(my_func, func)
+            if inspect.isfunction(func):
+                print('Функция:', func.__name__)
+                print('Результат:', func())
+        except AttributeError:
+            print('Такой функции не существует', func)
+    return
+
+
+runner()
+runner('euklid')
+runner('pair_elem')
+runner('languages', 'diff_words')
