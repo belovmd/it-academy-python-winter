9adea4990f1669af13ba76db419c6bc83c67fa59
Ioann87 <58548550+Ioann87@users.noreply.github.com>
2020-05-25 13:35:38 +0300

    Shastakou Ivan: additional task

--- src/test_prime_num.py
+++ src/test_prime_num.py
@@ -5,8 +5,7 @@ from divide import prime_number
 
 class TestFunction(unittest.TestCase):
     def test_prime_number(self):
-        """
-        Test for checking prime number function
-        """
+        """Test for checking prime number function"""
+
         num = prime_number(600851475143)
         self.assertEqual(max(num), 6857)
